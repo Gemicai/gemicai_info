24dd489dd47eb73c7f0e74f793633ce701a4f53c
Kevin Alberts <kevin@kevinalberts.nl>
2020-11-04 15:56:56 +0100

    Proposal 2 and new version of proposal 1

--- src/proposals.js
+++ src/proposals.js
@@ -4,5 +4,11 @@ export default [
         'anchor': "Neural-trees-A-novel-approach-for-the-classification-of-medical-images",
         'description': "To classify the general attributes of a medical images, a big challenge that we encounter is the high number of classes. With over 1300 classes to identify a single neural network is not capable of accurately all those classes. The proposed solution in to train neural networks speficly for detecting classes in certain subsets, to eventually create a tree bases model of neural networks. To put it simply, a tree where every node is a neural network. Research has show much potential in this approach. If proven to be succes full early application of this research could provide of much use for radiologists, they could guide in certain decision making processes, and automate certain task like hanging protocols. This research also could provide further insight to the applicability of AI in healthcare, especially medical imaging.",
         'file': "/proposals/Gemicai_proposal1.pdf"
-    }
+    },
+    {
+        'name': "Making optimal use of transfer learning for medical image classification",
+        'anchor': "Making-optimal-use-of-transfer-learning-for-medical-image-classification",
+        'description': "When classifying medical images, we often use pre-trained networks from the ImageNet competition like ResNet, Alexnet. All these networks are developed for images with color. Since medical images don’t have any color, the images are often gray-scaled. And passed to the neural network. Color transformation are used by radiologist worldwide to create more obvious distinctions for certain diagnosis. In this research we will investigate if color transformations can positively impact the performance of neural networks classifying medical images. This research will also investigate the effect of transfer learning for these networks.",
+        'file': "/proposals/Gemicai_proposal2.pdf"
+    },
 ]
