bf5506a1a84db2a98c7a83daa9e2c5c0a395db14
Kevin Alberts <kevin@kevinalberts.nl>
2020-11-04 14:17:36 +0100

    First research proposal

--- src/pages/Research.vue
+++ src/pages/Research.vue
@@ -2,7 +2,7 @@
   <q-page>
     <div class="q-pa-md">
       <h5 id="Further-Research" class="q-mt-none q-mb-md">Further Research</h5>
-      <p>
+      <p class="text-subtitle1">
         Gemicai as it is right now, is far from a perfect product. The first release is a 10-week project written for a
         Computer Science Bachelor design project, so of course not everything that would be needed for a library of this
         calibre is implemented. Because we do believe in the value of the library, we wrote some research proposals for
@@ -10,7 +10,7 @@
       </p>
       <div v-for="proposal of proposals" :key="proposal.name">
         <h6 :id="proposal.anchor" class="q-mt-none q-mb-sm">{{ proposal.name }}</h6>
-        <p class="text-subtitle1">{{ proposal.description }}</p>
+        <p class="text-weight-regular">{{ proposal.description }}</p>
         <PDFJSViewer :path="publicPath + 'lib/pdfjs/web/viewer.html'" :file="proposal.file"/>
         <q-space class="q-mb-lg" />
       </div>
--- src/proposals.js
+++ src/proposals.js
@@ -1,14 +1,8 @@
 export default [
     {
-        'name': "Proposal 1",
-        'anchor': "Proposal-1",
-        'description': "Short description for the first proposal. The idea is that there is a small description here.",
-        'file': "/proposals/dummy.pdf"
-    },
-    {
-        'name': "Proposal 2",
-        'anchor': "Proposal-2",
-        'description': "Test proposal 2",
-        'file': "/proposals/sample.pdf"
+        'name': "Neural trees: A novel approach for the classification of medical images",
+        'anchor': "Neural-trees-A-novel-approach-for-the-classification-of-medical-images",
+        'description': "To classify the general attributes of a medical images, a big challenge that we encounter is the high number of classes. With over 1300 classes to identify a single neural network is not capable of accurately all those classes. The proposed solution in to train neural networks speficly for detecting classes in certain subsets, to eventually create a tree bases model of neural networks. To put it simply, a tree where every node is a neural network. Research has show much potential in this approach. If proven to be succes full early application of this research could provide of much use for radiologists, they could guide in certain decision making processes, and automate certain task like hanging protocols. This research also could provide further insight to the applicability of AI in healthcare, especially medical imaging.",
+        'file': "/proposals/Gemicai_proposal1.pdf"
     }
 ]
